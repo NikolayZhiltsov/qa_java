b4cbfdbc617123217a4e57567c284243b0580b87
Nikolay Zhiltsov <nzhiltsov@yandex.ru>
2024-05-13 11:23:07 +0400

    Added test of exception's text in LionSexNegativeTest.

--- src/test/java/com/example/LionSexNegativeTest.java
+++ src/test/java/com/example/LionSexNegativeTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
 
 @RunWith(Parameterized.class)
 public class LionSexNegativeTest {
@@ -27,6 +30,16 @@ public class LionSexNegativeTest {
     @Test(expected = Exception.class)
     public void exceptionTest() throws Exception {
         new Lion(sex, feline);
+    }
+
+    @Test
+    public void exceptionTextTest() throws Exception {
+        Exception exception = assertThrows(Exception.class, () -> {
+            new Lion(sex, feline);
+        });
+        String expectedMessage = "Используйте допустимые значения пола животного - самец или самка";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 
 }
